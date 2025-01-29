ca411b6f95734cdb41934e9ff130c54477ad822c
Amir Reza Dalir <dalirnet@gmail.com>
2025-01-29 10:24:12 +0330

    feat: Update icons

--- make.js
+++ make.js
@@ -1,54 +1,31 @@
+import { join } from "path";
 import { writeFile } from "fs/promises";
 import { importDirectory } from "@iconify/tools/lib/import/directory";
 import { cleanupSVG } from "@iconify/tools/lib/svg/cleanup";
 import { runSVGO } from "@iconify/tools/lib/optimise/svgo";
 import { parseColors, isEmptyColor } from "@iconify/tools/lib/colors/parse";
 
-async function save(icons) {
-    const content = JSON.stringify(icons.export(), null, 4);
+const icons = await importDirectory("src");
 
-    await writeFile(`output/${icons.prefix}.json`, content, "utf8");
+for (const name of icons.list()) {
+    const svg = icons.toSVG(name);
+    if (!svg) {
+        continue;
+    }
+
+    try {
+        cleanupSVG(svg);
+        parseColors(svg, {
+            defaultColor: "currentColor",
+            callback: (_, colorStr, color) => {
+                return !color || isEmptyColor(color) ? colorStr : "currentColor";
+            },
+        });
+
+        runSVGO(svg);
+
+        await writeFile(join("dist", `${name}.svg`), svg.toMinifiedString());
+    } catch ({ message }) {
+        console.error(`Error parsing ${name}:`, message);
+    }
 }
-
-async function make() {
-    const icons = await importDirectory("svg", {
-        prefix: "dx",
-    });
-
-    icons.forEach((name, type) => {
-        if (type !== "icon") {
-            return;
-        }
-
-        const svg = icons.toSVG(name);
-        if (!svg) {
-            icons.remove(name);
-
-            return;
-        }
-
-        try {
-            cleanupSVG(svg);
-            parseColors(svg, {
-                defaultColor: "currentColor",
-                callback: (_, colorStr, color) => {
-                    return !color || isEmptyColor(color) ? colorStr : "currentColor";
-                },
-            });
-
-            runSVGO(svg);
-        } catch ({ message }) {
-            console.error(`Error parsing ${name}:`, message);
-
-            icons.remove(name);
-
-            return;
-        }
-
-        icons.fromSVG(name, svg);
-    });
-
-    await save(icons);
-}
-
-make();
