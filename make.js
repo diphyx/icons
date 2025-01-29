24195ffe83d9dcd2a0ac1da3c948fecdab0e1d6f
Amir Reza Dalir <dalirnet@gmail.com>
2025-01-29 19:31:47 +0330

    feat: Update icons

--- make.js
+++ make.js
@@ -3,7 +3,6 @@ import { writeFile } from "fs/promises";
 import { importDirectory } from "@iconify/tools/lib/import/directory";
 import { cleanupSVG } from "@iconify/tools/lib/svg/cleanup";
 import { runSVGO } from "@iconify/tools/lib/optimise/svgo";
-import { parseColors, isEmptyColor } from "@iconify/tools/lib/colors/parse";
 
 const icons = await importDirectory("src");
 
@@ -15,13 +14,6 @@ for (const name of icons.list()) {
 
     try {
         cleanupSVG(svg);
-        parseColors(svg, {
-            defaultColor: "currentColor",
-            callback: (_, colorStr, color) => {
-                return !color || isEmptyColor(color) ? colorStr : "currentColor";
-            },
-        });
-
         runSVGO(svg);
 
         await writeFile(join("dist", `${name}.svg`), svg.toMinifiedString());
